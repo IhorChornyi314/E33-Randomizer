cc7d314e068d4aa5652e54ba52a6fee0f2637745
iamwyza <iamwyza@gmail.com>
2026-05-28 21:32:50 -0500

    Tweaked a few more styles and got the design time view better for the randomize enemies tab.  I think it's basically complete at this point minus any tweaks.

--- E33Randomizer/RandomizeEnemiesTab.axaml.cs
+++ E33Randomizer/RandomizeEnemiesTab.axaml.cs
@@ -20,3 +20,27 @@ public partial class RandomizeEnemiesTab : UserControl
         App.GetMainWindow().OpenEditObjectsButton_Click(sender, e);
     }
 }
+
+// Design Time Settings DataContext
+public static class DesignEnemiesSettingsViewModel
+{
+    public static SettingsViewModel SettingsViewModel => new()
+    {
+        RandomizeEnemies = true,
+        RandomizeEncounterSizes = true,
+        EncounterSizeOne = true,
+        EncounterSizeTwo = true,
+        EncounterSizeThree = true,
+        ChangeSizeOfNonRandomizedEncounters = true,
+        RandomizeMerchantFights = true,
+        IncludeCutContentEnemies = true,
+        EnableEnemyOnslaught = true,
+        EnemyOnslaughtAdditionalEnemies = 1,
+        EnemyOnslaughtEnemyCap = 4,
+        EnsureBossesInBossEncounters = true,
+        ReduceBossRepetition = true,
+        NoSimonP2BeforeLune = true,
+        RandomizeAddedEnemies = true
+    };
+        
+}
