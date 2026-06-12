d90983704d453c2725f3378924cb2f60c1f89e88
iamwyza <iamwyza@gmail.com>
2026-06-12 17:03:35 -0500

    Fix file permissions on linux during launch to ensure retoc and uesave can be executed.

--- E33Randomizer/Program.cs
+++ E33Randomizer/Program.cs
@@ -10,17 +10,40 @@ public sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        FixExecutableBitOnLinux();
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
+
+    private static void FixExecutableBitOnLinux()
+    {
+        if (!OperatingSystem.IsLinux()) return;
+        
+        var mode = File.GetUnixFileMode($"{Directory.GetCurrentDirectory()}/retoc");
+        if (!mode.HasFlag(UnixFileMode.UserExecute))
+        {
+            mode |= UnixFileMode.UserExecute;
+            File.SetUnixFileMode($"{Directory.GetCurrentDirectory()}/retoc", mode);    
+        }
+            
+            
+        mode = File.GetUnixFileMode($"{Directory.GetCurrentDirectory()}/uesave");
+        if (!mode.HasFlag(UnixFileMode.UserExecute))
+        {
+            mode |= UnixFileMode.UserExecute;
+            File.SetUnixFileMode($"{Directory.GetCurrentDirectory()}/uesave", mode);    
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    private static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
-            .ConfigureFonts(manager =>
-            {
-                manager.AddFontCollection(new FontCollection());
-            })
-           // .LogToTextWriter(File.CreateText("log.txt"), LogEventLevel.Verbose)
-            .WithInterFont(); 
+            .ConfigureFonts(manager => { manager.AddFontCollection(new FontCollection()); })
+            // .LogToTextWriter(File.CreateText("log.txt"), LogEventLevel.Verbose)
+            .WithInterFont();
+    }
 }
